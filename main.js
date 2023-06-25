3d72b6e4d2ad24d614e1c0e6bae5cb6136883320
BonnieM66 <115851841+BonnieM66@users.noreply.github.com>
2023-06-24 18:51:47 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist = 0;
+songStatus = "";
+
+function preload() {
+    song1 = loadSound("music1.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log("posenet is initialized!");
+}
+
+function gotPoses(results) {
+    if(results.length > 0)
+    {
+        console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
+}
+
+function draw() {
+    image(video, 0, 0, 600, 500);
+
+    songStatus = song1.isPlaying();
+    console.log(songStatus);
+
+    fill("#4d9e63");
+    stroke("#4d9e63");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+
+        song2.stop();
+
+        if(songStatus == false)
+        {
+            song1.play();
+            document.getElementById("song").innerHTML = "song playing: harry potter theme song";
+        }
+    }
+
+}
